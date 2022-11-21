619672d61f7480a92d2c10f19141f7e897d015d5
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2022-11-21 22:57:44 +0000

    Changes to be committed:

--- myOS/OS/kernel2.sh
+++ myOS/OS/kernel2.sh
@@ -4,9 +4,8 @@ cat bootloader.bin OS.bin > os.bin
 sudo dd if=os.bin of=/dev/sdb conv=notrunc
 rm bootloader.bin
 rm OS.bin
-rm ~/Documents/'my OS'/myOS/dumps/os.bin
-cp ~/Documents/'my OS'/myOS/OS/os.bin ~/Documents/'my OS'/myOS/dumps/os.bin
+rm /workspaces/myOS/myOS/dumps/os.bin
+cp /workspaces/myOS/myOS/OS/os.bin /workspaces/myOS/myOS/dumps/os.bin
 rm os.bin
-cd
-cd Documents/'my OS'/myOS/dumps
+cd /workspaces/myOS/myOS/dumps/
 ./dumps.sh
