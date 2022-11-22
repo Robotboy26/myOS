60259e909e42f07e8cfd944bc74d274073776ddf
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2022-11-22 16:44:25 +0000

    Changes to be committed:

--- myOS/OS/bootloader.asm
+++ myOS/OS/bootloader.asm
@@ -1,31 +1,5 @@
-    ; Check if CPUID is supported by attempting to flip the ID bit (bit 21) in
-    ; the FLAGS register. If we can flip it, CPUID is available.
- 
-    ; Copy FLAGS in to EAX via stack
-    pushfd
-    pop eax
- 
-    ; Copy to ECX as well for comparing later on
-    mov ecx, eax
- 
-    ; Flip the ID bit
-    xor eax, 1 << 21
- 
-    ; Copy EAX to FLAGS via the stack
-    push eax
-    popfd
- 
-    ; Copy FLAGS back to EAX (with the flipped bit if CPUID is supported)
-    pushfd
-    pop eax
- 
-    ; Restore FLAGS from the old version stored in ECX (i.e. flipping the ID bit
-    ; back if it was ever flipped).
-    push ecx
-    popfd
- 
-    ; Compare EAX and ECX. If they are equal then that means the bit wasn't
-    ; flipped, and CPUID isn't supported.
-    xor eax, ecx
-    jz .NoCPUID
-    ret
+
+jmp $
+
+times 510 - ($ - $$) db 0 ; padding to fill the bootsector up to 512 bytes
+dw 0xAA55 ; boot signiture (tells the computer that this is the boot sector)
