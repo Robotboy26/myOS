40ce132a791a69c1dfec398b4da518d61a2abc5d
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2022-11-22 16:59:01 +0000

    Changes to be committed:

--- myOS/OS/bootloader.asm
+++ myOS/OS/bootloader.asm
@@ -1,5 +1,9 @@
 
-jmp $
+mov ah, 0x0e
+
+mov al, 'h'
+
+int 0x10
 
 times 510 - ($ - $$) db 0 ; padding to fill the bootsector up to 512 bytes
 dw 0xAA55 ; boot signiture (tells the computer that this is the boot sector)
--- myOS/qemu.sh
+++ myOS/qemu.sh
@@ -1,1 +1,2 @@
 sudo qemu-system-x86_64 -machine accel=kvm:tcg -m 1024 /dev/sdb -nographic
+# stop qemu press ctrl-a then x
