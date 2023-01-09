317336aa50f5457e9b91ffcbdeb57b785ededc4b
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-01-09 04:33:02 +0000

    Changes to be committed:

--- myOS/OS/bootloader.asm
+++ myOS/OS/bootloader.asm
@@ -73,6 +73,14 @@ bootsectorExtended:
 
 call enableA20
 
+setupIDT:
+  mov   ax, 0x0000      ; Have es:di point to 0000:0000
+  mov   es, ax
+  mov   di, 0
+  mov   cx, 2048        ; Write 2048 zeroes
+  rep   stosb           ; since the 2048 has 256 entries of 8 bytes.
+  ret
+
 IDT:
     dw 2048  ; Size of IDT (256 entries of 8 bytes)
     dd 0x0   ; Linear address of IDT
@@ -83,6 +91,7 @@ loadIDT:
 
 A20Enabled:
 
+call setupIDT
 call loadIDT
 
 beginProtected:
